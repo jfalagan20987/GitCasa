d6147bdec24659f0a26d7242b4c6c97e93bda7a7
Josep <josepfg94@gmail.com>
2024-10-22 11:33:36 +0200

    Tercer commit

--- PruebaExamen.java
+++ PruebaExamen.java
@@ -1,4 +1,4 @@
 //Esto es una prueba de cara al examen, que tendrá lugar el
 jueves 24 de cotubre de 2024
 
-//El examen será de 15:30 a 17:20
+//El examen concurrírá desde las 15:30 a las 17:20 horas
