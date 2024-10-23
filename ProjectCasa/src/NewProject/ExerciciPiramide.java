81e8fc87b5953200418c3852eb4ca5c0cf5a46fe
Josep <jfalagan20987@iesjoanramis.org>
2024-10-23 11:03:42 +0200

    Prova commit Eclipse

---
+++ ProjectCasa/src/NewProject/ExerciciPiramide.java
@@ -0,0 +1,19 @@
+package NewProject;
+
+public class ExerciciPiramide {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int files = 5;
+		
+		for (int i = 1 ; i <= files ; i++) {
+			for (int j = 1 ; j<= i ; j++) {
+				System.out.print("a ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ ProjectCasa/src/NewProject/PruebaGit.java
@@ -0,0 +1,12 @@
+package NewProject;
+
+public class PruebaGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Prueba para repositorio Git
+		
+	}
+
+}
