2f0bcf98f214fcbcccef08eba1516dacf1c36386
Josep <jfalagan20987@iesjoanramis.org>
2024-10-23 11:09:54 +0200

    Commit a testing

---
+++ ProjectCasa/src/NewProject/ProvaBranques.java
@@ -0,0 +1,12 @@
+package NewProject;
+
+public class ProvaBranques {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Estic fent una prova amb noves branques
+
+	}
+
+}
