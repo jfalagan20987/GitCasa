1b79e1bd2fa95666d25d2ce1bebd7d39824d09ea
Josep <jfalagan20987@iesjoanramis.org>
2024-10-24 10:20:51 +0200

    Commit a master de prueba de examen

--- ProvaExamen.java
+++ ProvaExamen.java
@@ -1,4 +1,5 @@
+//Editamos en master
 //Esto es una prueba de cara al examen, que tendrá lugar el
 jueves 24 de cotubre de 2024
 
-//El examen concurrírá desde las 15:30 a las 17:20 horas
+//El examen concurrírá desde las 17:20 a las 19:40 horas
